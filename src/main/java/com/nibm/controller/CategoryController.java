1b777f7eae4692b12f51a6ef898d2a4842bda850
rockywalter <vishwa.danan@gmail.com>
2021-08-30 21:09:31 +0530

    Added category APIs

--- src/main/java/com/nibm/controller/CategoryController.java
+++ src/main/java/com/nibm/controller/CategoryController.java
@@ -1,8 +1,40 @@
 package com.nibm.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nibm.model.Category;
+import com.nibm.repository.CategoryService;
 
 @RestController
 public class CategoryController {
+
+	@Autowired
+	CategoryService catservice;
+
+	@GetMapping("/allcategory")
+	public List<Category> getAllCat() {
+		return catservice.findAll();
+
+	}
+
+	@GetMapping("/category")
+	public Optional<Category> findById(@RequestBody Category category) {
+		return catservice.getcategory(category);
+
+	}
+
+	@PostMapping("/category")
+	public Category addCategory(@RequestBody Category category) {
+		return catservice.addOrUpdateCategory(category);
+
+	}
 
 }
